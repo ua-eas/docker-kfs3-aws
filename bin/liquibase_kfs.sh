15dfc8509e6854f3355a34e8db75d550f2e650aa
Jennifer Hard <jshard@email.arizona.edu>
2016-03-07 16:29:15 -0700

    UAF-2276 adding liquibase jar file for 3.3.5 and updating liquibase_kfs.sh to call it

--- bin/liquibase_kfs.sh
+++ bin/liquibase_kfs.sh
@@ -11,7 +11,7 @@ LIQUIBASE_DB_USERNAME=$(grep "kfs.datasource.username" $KFS_CONFIG_FILE | sed -e
 LIQUIBASE_DB_PASSWORD=$(grep "kfs.datasource.password" $KFS_CONFIG_FILE | sed -e 's/^kfs\.datasource\.password=//')
 LIQUIBASE_DB_URL=$(grep "kfs.datasource.url" $KFS_CONFIG_FILE | sed -e 's/^kfs\.datasource\.url=//')
 
-exec /usr/bin/java -jar $LIQUIBASE_HOME/liquibase-1.9.5.jar \
+exec /usr/bin/java -jar $LIQUIBASE_HOME/liquibase-3.3.5.jar \
 --url="$LIQUIBASE_DB_URL" \
 --username=$LIQUIBASE_DB_USERNAME \
 --password=$LIQUIBASE_DB_PASSWORD \
